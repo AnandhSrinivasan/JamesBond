6030eedd1013691841959688c95498eac8f1497c
Dell <Dell@MuruganandhSrinivasan>
2023-10-29 20:26:22 +0530

    JamesBond_Commit

---
+++ Object Repository/SignUp_PHP_Travels_/Page_Book Your Free Demo Now - Phptravels/svg_lc-1mpchac.rs
@@ -0,0 +1,75 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<WebElementEntity>
+   <description></description>
+   <name>svg_lc-1mpchac</name>
+   <tag></tag>
+   <elementGuidId>6e2b8acb-8a43-4e86-8cdb-f7cedbf7eb64</elementGuidId>
+   <selectorCollection>
+      <entry>
+         <key>XPATH</key>
+         <value></value>
+      </entry>
+      <entry>
+         <key>CSS</key>
+         <value>button.e1mwfyk10.lc-1iovl8i.e1m5b1js0 > svg.lc-1mpchac</value>
+      </entry>
+   </selectorCollection>
+   <selectorMethod>XPATH</selectorMethod>
+   <useRalativeImagePath>true</useRalativeImagePath>
+   <webElementProperties>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>tag</name>
+      <type>Main</type>
+      <value>svg</value>
+      <webElementGuid>d570df03-9c08-4adf-8dc8-21f889100ec6</webElementGuid>
+   </webElementProperties>
+   <webElementProperties>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>color</name>
+      <type>Main</type>
+      <value>inherit</value>
+      <webElementGuid>efac77b2-2132-4b5f-8c4a-9e3271c81cb0</webElementGuid>
+   </webElementProperties>
+   <webElementProperties>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>viewBox</name>
+      <type>Main</type>
+      <value>0 0 32 32</value>
+      <webElementGuid>6c7f2e2e-30e4-4ecb-b9dd-74fe783f8cf0</webElementGuid>
+   </webElementProperties>
+   <webElementProperties>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>aria-hidden</name>
+      <type>Main</type>
+      <value>true</value>
+      <webElementGuid>7379b425-0776-472e-988f-f996c1c8f958</webElementGuid>
+   </webElementProperties>
+   <webElementProperties>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>class</name>
+      <type>Main</type>
+      <value>lc-1mpchac</value>
+      <webElementGuid>35f1e965-886c-4a76-a3a5-dc91defc5d81</webElementGuid>
+   </webElementProperties>
+   <webElementProperties>
+      <isSelected>true</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath</name>
+      <type>Main</type>
+      <value>id(&quot;widget-global-rzf76zrka&quot;)/div[@class=&quot;lc-1dvk0j5 edvz03i2 lc-12vmhyu-exit lc-12vmhyu-exit-active&quot;]/div[@class=&quot;lc-jy8v6l edvz03i1&quot;]/div[@class=&quot;lc-1qlri12 e1ohfhv1&quot;]/button[@class=&quot;e1mwfyk10 lc-1iovl8i e1m5b1js0&quot;]/svg[@class=&quot;lc-1mpchac&quot;]</value>
+      <webElementGuid>22a870ab-c3bb-4c77-95d0-308cc9dd8c25</webElementGuid>
+   </webElementProperties>
+   <webElementProperties>
+      <isSelected>true</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>ref_element</name>
+      <type>Main</type>
+      <value>Object Repository/SignUp_PHP_Travels_/Page_Book Your Free Demo Now - Phptravels/iframe_chat-widget</value>
+      <webElementGuid>fd17792f-d14b-49ce-ae94-ab8bab550845</webElementGuid>
+   </webElementProperties>
+</WebElementEntity>
